feb426fac22c12abd0c79445ab5256ccc6d0a84f
Fomin Vladimir <fomin.v97@yandex.ru>
2023-09-12 23:10:02 +0300

    COD-13 Инициализация проекта

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,7 @@
-import { useEffect } from 'react'
-import './App.css'
+import './App.css';
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
-
-    fetchServerData()
-  }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+  return <div>Привет, мир!</div>;
 }
 
-export default App
+export default App;
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
-dotenv.config()
+import { fileURLToPath, URL } from 'url';
+
+import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+import react from '@vitejs/plugin-react';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +15,29 @@ export default defineConfig({
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@app/api', replacement: fileURLToPath(new URL('./src/api', import.meta.url)) },
+      {
+        find: '@app/components',
+        replacement: fileURLToPath(new URL('./src/components', import.meta.url)),
+      },
+      { find: '@app/hooks', replacement: fileURLToPath(new URL('./src/hooks', import.meta.url)) },
+      {
+        find: '@app/controllers',
+        replacement: fileURLToPath(new URL('./src/controllers', import.meta.url)),
+      },
+      { find: '@app/pages', replacement: fileURLToPath(new URL('./src/pages', import.meta.url)) },
+      {
+        find: '@app/services',
+        replacement: fileURLToPath(new URL('./src/services', import.meta.url)),
+      },
+      { find: '@app/store', replacement: fileURLToPath(new URL('./src/store', import.meta.url)) },
+      { find: '@app/types', replacement: fileURLToPath(new URL('./src/types', import.meta.url)) },
+      {
+        find: '@app/utils',
+        replacement: fileURLToPath(new URL('./src/utils', import.meta.url)),
+      },
+    ],
+  },
+});
