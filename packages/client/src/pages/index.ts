686a06e1c12e158026a27e937fadc93d159bcc18
Pavel Martynov <osoko007@gmail.com>
2023-09-19 00:25:02 +0300

    Use Link from components, heading font-weight

--- packages/client/src/pages/index.ts
+++ packages/client/src/pages/index.ts
@@ -3,6 +3,6 @@ export { GamePage } from './game/Game';
 export { LeaderboardPage } from './leaderboard/Leaderboard';
 export { LoginPage } from './login/Login';
 export { MainPage } from './main/Main';
-export { NotFoundPage } from './notFound/NotFoundPage';
+export { NotFoundPage } from './notFound/NotFound';
 export { ProfilePage } from './profile/Profile';
 export { RegisterPage } from './register/Register';
--- packages/client/src/pages/notFound/NotFound.tsx
+++ packages/client/src/pages/notFound/NotFound.tsx
@@ -1,35 +1,16 @@
-import {
-  Box,
-  Button,
-  Center,
-  Link as ChakraLink,
-  Flex,
-  Heading,
-  Image,
-  Text,
-} from '@chakra-ui/react';
-import { Link as ReactRouterLink } from 'react-router-dom';
+import { Box, Button, Center, Flex, Heading, Image, Text } from '@chakra-ui/react';
 
-import { GoBackIcon } from '@app/components';
+import { GoBackIcon, Link } from '@app/components';
 
 import Robot from '../../assets/images/robot.png';
-import './NotFoundPage.css';
-
-function CustomLink() {
-  return (
-    <ChakraLink as={ReactRouterLink} to="/" color="black" className="not-found__go-back-link">
-      <GoBackIcon />
-      Go back
-    </ChakraLink>
-  );
-}
+import './NotFound.css';
 
 export function NotFoundPage() {
   return (
     <Center h="100vh">
       <Flex flexDirection="column" alignItems="center">
         <Flex alignItems="center">
-          <Heading as="h1" size="4xl" className="not-found__title">
+          <Heading as="h1" size="4xl" className="not-found__title" fontWeight={400}>
             ERROR
             <Flex>
               <Text>4</Text>
@@ -40,7 +21,10 @@ export function NotFoundPage() {
           <Box boxSize="md" position="relative" mb={20}>
             <Image src={Robot} alt="ERROR ROBOT" align="center" />
             <Button variant="link" className="not-found__go-back-button">
-              <CustomLink />
+              <Link className="not-found__go-back-link" to="/">
+                <GoBackIcon />
+                Go back
+              </Link>
             </Button>
           </Box>
         </Flex>
