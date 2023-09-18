575996c67ab676cc61ff1a7e9b64aebb89d93ab5
Pavel Martynov <osoko007@gmail.com>
2023-09-18 22:59:14 +0300

    Installed new styles based on recommendations from PR

--- packages/client/src/pages/notFound/NotFoundPage.tsx
+++ packages/client/src/pages/notFound/NotFoundPage.tsx
@@ -1,4 +1,13 @@
-import { Box, Button, Center, Link as ChakraLink, Flex, Image, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Center,
+  Link as ChakraLink,
+  Flex,
+  Heading,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
 import { GoBackIcon } from '@app/components';
@@ -20,14 +29,14 @@ export function NotFoundPage() {
     <Center h="100vh">
       <Flex flexDirection="column" alignItems="center">
         <Flex alignItems="center">
-          <Text className="not-found__title">
+          <Heading as="h1" size="4xl" className="not-found__title">
             ERROR
             <Flex>
               <Text>4</Text>
-              <Box className="not-found__code_central">0</Box>
+              <Text className="not-found__code_central">0</Text>
               <Text>4</Text>
             </Flex>
-          </Text>
+          </Heading>
           <Box boxSize="md" position="relative" mb={20}>
             <Image src={Robot} alt="ERROR ROBOT" align="center" />
             <Button variant="link" className="not-found__go-back-button">
