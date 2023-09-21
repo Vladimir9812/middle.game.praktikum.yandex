86bc4ad2426dd9a2b702fad0ade62d4015ae2f9b
Pavel Martynov <osoko007@gmail.com>
2023-09-21 11:55:01 +0300

    Added 503 page

--- packages/client/src/pages/serviceUnavailable/ServiceUnavailable.tsx
+++ packages/client/src/pages/serviceUnavailable/ServiceUnavailable.tsx
@@ -3,19 +3,19 @@ import { Box, Button, Center, Flex, Heading, Image, Text } from '@chakra-ui/reac
 import { GoBackIcon, Link } from '@app/components';
 
 import Robot from '../../assets/images/robot.png';
-import './NotFound.css';
+import './ServiceUnavailable.css';
 
-export function NotFoundPage() {
+export function ServiceUnavailable() {
   return (
     <Center h="100vh">
       <Flex flexDirection="column" alignItems="center">
         <Flex alignItems="center">
-          <Heading as="h1" size="4xl" className="not-found__title" fontWeight={400}>
+          <Heading as="h1" size="4xl" className="service-unavailable__title" fontWeight={400}>
             ERROR
             <Flex>
-              <Text>4</Text>
-              <Text className="not-found__code_central">0</Text>
-              <Text>4</Text>
+              <Text>5</Text>
+              <Text className="service-unavailable__code_central">0</Text>
+              <Text>3</Text>
             </Flex>
           </Heading>
           <Box boxSize="md" position="relative">
