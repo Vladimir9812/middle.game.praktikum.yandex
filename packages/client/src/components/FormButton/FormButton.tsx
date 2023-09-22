9f46e6413b586c124ba4e7377c551d9f7ac50b0b
Konstantin Brovtsev <brovan@yandex.ru>
2023-09-22 07:45:52 +0300

    fix: delete redundant log

--- packages/client/src/components/FormButton/FormButton.tsx
+++ packages/client/src/components/FormButton/FormButton.tsx
@@ -7,7 +7,6 @@ type Properties = {
 };
 
 export function FormButton({ isDisabled, label, type }: Properties) {
-  console.log(isDisabled);
   return (
     <Button isDisabled={isDisabled} size="lg" colorScheme="red" type={type ?? 'submit'}>
       {label}
