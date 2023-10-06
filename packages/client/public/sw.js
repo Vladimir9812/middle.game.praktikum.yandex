74b9ce0928af2d470301d738f9a935cd15cca610
Pavel Martynov <osoko007@gmail.com>
2023-10-06 12:18:26 +0300

    Added serviceWorker to the project -file caching -changing the cache variable at build time -wrote a plugin to change files during assembly
    feature/COD-50

---
+++ packages/client/public/sw.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = 'cache-name-v1';
+
+const URLS = ['/'];
+
+function timeout(delay) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      reject(
+        new Response('', {
+          status: 408,
+          statusText: 'Request timed out.',
+        }),
+      );
+    }, delay);
+  });
+}
+
+self.addEventListener('install', (event) => {
+  console.log('install');
+  self.skipWaiting();
+  event
+    .waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        cache.addAll(URLS); //загрузка тяжелых данных (дополнительные данные)
+        return cache.addAll(URLS); //вернет важные данные в метод waitUntil (необходимые данные)
+      }),
+    )
+    .catch((err) => console.log(err));
+});
+
+self.addEventListener('fetch', function (event) {
+  const request = event.request;
+
+  if (request.method === 'GET') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function (cache) {
+        return Promise.race([timeout(300), fetch(event.request)])
+          .then(function (response) {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => {
+            return caches.match(event.request).then(function (response) {
+              if (response) {
+                return response;
+              }
+            });
+          });
+      }),
+    );
+  } else {
+    event.respondWith(fetch(request));
+  }
+});
+
+self.addEventListener('activate', function (event) {
+  console.log('activate');
+  if (self.clients && clients.claim) {
+    clients.claim();
+  }
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => {
+            return name !== CACHE_NAME;
+          })
+          .map((name) => caches.delete(name)),
+      );
+    }),
+  );
+});
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -10,6 +10,23 @@ import './index.css';
 
 const store = setupStore();
 
+function startServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register(import.meta.env.PROD ? `${import.meta.env.BASE_URL}sw.js` : '/sw.js')
+        .then((registration) => {
+          /* eslint-disable-next-line no-console */
+          console.log('ServiceWorker registration successful with scope:', registration.scope);
+        })
+        .catch((error: string) => {
+          /* eslint-disable-next-line no-console */
+          console.log('ServiceWorker registration failed:', error);
+        });
+    });
+  }
+}
+
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
@@ -17,3 +34,5 @@ ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
     </Provider>
   </React.StrictMode>,
 );
+
+startServiceWorker();
