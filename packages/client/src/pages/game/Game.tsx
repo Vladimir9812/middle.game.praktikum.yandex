e0ca786802c4d14fb913d4f0d3e4b622d9a93d4c
nikitandro <nik.vish.93@mail.ru>
2023-10-07 11:34:47 +0500

    feat: working on game page state

--- packages/client/src/pages/game/Game.tsx
+++ packages/client/src/pages/game/Game.tsx
@@ -1,11 +1,29 @@
-// type Props = {};
+import { useState } from 'react';
+import { Box } from '@chakra-ui/react';
 
 import { EngineCanvas } from '@app/components';
 
+enum GameState {
+  NotStarted,
+  Started,
+  Stopped,
+}
+
+function NotStartedPageView() {
+  return <Box>Not Started</Box>;
+}
+
+function StoppedPageView() {
+  return <Box>Stopped</Box>;
+}
+
+const PageView = {
+  [GameState.NotStarted]: <NotStartedPageView />,
+  [GameState.Started]: <EngineCanvas />,
+  [GameState.Stopped]: <StoppedPageView />,
+};
+
 export function GamePage() {
-  return (
-    <div>
-      <EngineCanvas />
-    </div>
-  );
+  const [gameState, setGameState] = useState<GameState>(GameState.NotStarted);
+  return PageView[gameState];
 }
---
+++ packages/client/src/store/slices/ScoreSlice/ScoreSlice.ts
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type ScoreState = {
+  score: number;
+};
+
+const initialState: ScoreState = {
+  score: 0,
+};
+
+export const scoreSlice = createSlice({
+  name: 'score',
+  initialState,
+  reducers: {
+    setScore(state, action: PayloadAction<number>) {
+      // eslint-disable-next-line no-param-reassign
+      state.score = action.payload;
+    },
+  },
+});
