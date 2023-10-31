bf52c39ceb50f3dc42d6d4343fe4028508316cfd
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-10-31 11:05:11 +0300

    change import in store

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -4,3 +4,4 @@ export { scoreActions } from './slices/ScoreSlice/ScoreSlice';
 export * from './slices/UserActionCreators';
 export { userSliceActions } from './slices/UserSlice';
 export * from './store';
+export { sendScore, getLeaderboard } from './slices/GameActionCreators';
